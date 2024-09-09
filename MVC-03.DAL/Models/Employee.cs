cbf785ddcd427b137af7d4478f5378bbeb5b3be0
Mohab662 <mohabmbelkan@gmail.com>
2024-09-10 00:12:26 +0300

    All Assignment

---
+++ MVC-03.DAL/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_03.DAL.Models
+{
+    public enum Gender
+    {
+        [EnumMember(Value = "Male")]
+        Male =1,
+        [EnumMember(Value = "Female")]
+        Female =2
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+        public int? Age { get; set; }
+        public bool IsActive { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime HireDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
--- MVC-03.PL/Controllers/DepartmentController.cs
+++ MVC-03.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using MVC_03.DAL.Models;
 using MVC_03.PLL.Interfaces;
 using MVC_03.PLL.Repositries;
 
@@ -7,14 +10,136 @@ namespace MVC_03.PL.Controllers
     public class DepartmentController : Controller
     {
         private readonly IDepartmentRepository departmentRepository;
-        public DepartmentController(IDepartmentRepository departmentRepository)
+        private readonly IWebHostEnvironment _env;
+
+        public DepartmentController(IDepartmentRepository departmentRepository,IWebHostEnvironment _env)
         {
             this.departmentRepository= departmentRepository;
+            this._env = _env;
         }
         public IActionResult Index()
         {
            var departments=departmentRepository.GetAll();
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+               var count= departmentRepository.Add(department);
+                if (count>0)
+                {
+                   return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
+        }
+        
+        [HttpGet]
+        public IActionResult Details(int? id,string viewName= "Details")
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+            var department=departmentRepository.GetById(id.Value);
+            if (department==null)
+            {
+                return NotFound();
+            }
+            return View(viewName,department);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            //if (!id.HasValue)
+            //{
+            //    return BadRequest();
+            //}
+            //var department = departmentRepository.GetById(id.Value);
+            //if (department == null)
+            //{
+            //    return NotFound();
+            //}
+            //return View(department);
+            return Details(id,"Edit");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int? id,Department department)
+        {
+            if (id!=department.Id)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            return View(department);
+
+            try 
+            {
+                var count = departmentRepository.Update(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch(System.Exception e) 
+            {
+                if (_env.IsDevelopment())
+                {
+                    ModelState.AddModelError(string.Empty, e.Message);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Failed To Update");
+                }
+            }
+            return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            //if (!id.HasValue)
+            //{
+            //    return BadRequest();
+            //}
+            //var department = departmentRepository.GetById(id.Value);
+            //if (department == null)
+            //{
+            //    return NotFound();
+            //}
+            //return View(department);
+            return Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Department department)
+        {
+            try
+            {
+                var count = departmentRepository.Delete(department);
+                    return RedirectToAction(nameof(Index));
+            }
+            catch (System.Exception e)
+            {
+                if (_env.IsDevelopment())
+                {
+                    ModelState.AddModelError(string.Empty, e.Message);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Failed To Delete");
+                }
+            }
+            return View(department);
         }
     }
 }
